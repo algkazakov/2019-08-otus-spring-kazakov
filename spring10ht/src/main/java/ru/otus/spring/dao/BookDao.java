2b3cdd4d57ed5e89ec6a259a06bd015d6c49f2a9
Alexander G. Kazakov <akazakov@diasoft.ru>
2019-11-01 12:47:56 +0300

    Resolve issues

--- spring10ht/src/main/java/ru/otus/spring/dao/BookDao.java
+++ spring10ht/src/main/java/ru/otus/spring/dao/BookDao.java
@@ -1,17 +1,8 @@
 package ru.otus.spring.dao;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.spring.domain.Book;
 
-import java.util.Optional;
-
 public interface BookDao extends CrudRepository<Book, Long> {
-    @Override
-    @Query("select distinct b from Book b join fetch b.authors join fetch b.genres order by b.id")
-    Iterable<Book> findAll();
 
-    @Override
-    @Query("select b from Book b join fetch b.authors join fetch b.genres where b.id = ?1")
-    Optional<Book> findById(Long id);
 }
